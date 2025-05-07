dd6ea83be4116409dc84b4ea6744c8759bd139df
zhou-wang-git <184232456@qq.com>
2025-05-07 22:08:15 +0800

    更新文件

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// vite.config.js
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
- base: '/GameHouse/',  // 设置为你的子目录路径
-})
+  base: './', // 建议本地测试用相对路径
+  build: {
+    outDir: 'dist',
+    sourcemap: true // 便于调试
+  }
+});
