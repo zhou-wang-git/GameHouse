0e46ab60edf74c72d036b82dab3d8ec84f4c933a
zhou-wang-git <184232456@qq.com>
2025-05-07 23:02:06 +0800

    修复路径和重定向

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
-  base: './GameHouse/', // 建议本地测试用相对路径
+  base: process.env.NODE_ENV === 'production' ? '/GameHouse/' : './',
   build: {
     outDir: 'dist',
-    sourcemap: true // 便于调试
+    sourcemap: true
   }
 });
