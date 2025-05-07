3168ff2d4b08cd21bd54ed8883bd270c131ac995
zhou-wang-git <184232456@qq.com>
2025-05-07 20:24:48 +0800

    fix: set correct base path and rebuild

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/GameHouse/', // 你的仓库名，注意前后有斜杠
+  base: '/', // 你的仓库名，注意前后有斜杠
   plugins: [vue()],
 })
